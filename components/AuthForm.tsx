9e25c568c377fdcc2e6228e92593bd0b123db4ae
Lushito <luisdevcol@gmail.com>
2024-05-26 19:06:07 -0500

    chore: Refactor AuthForm component for improved readability and reusability

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -76,20 +76,35 @@ const AuthForm = ({ type }: { type: string }) => {
                 label="Password"
                 placeholder="Enter Your password"
               />
-              <Button className="form-btn" type="submit">
-                {loading ? (
-                  <>
-                    <Loader2 size={20} className="animate-spin" /> &nbsp;
-                    Loading...
-                  </>
-                ) : type === "sign-in" ? (
-                  "Sign in"
-                ) : (
-                  "Sign up"
-                )}
-              </Button>
+              <div className="flex flex-col gap-4">
+                <Button className="form-btn" type="submit">
+                  {loading ? (
+                    <>
+                      <Loader2 size={20} className="animate-spin" /> &nbsp;
+                      Loading...
+                    </>
+                  ) : type === "sign-in" ? (
+                    "Sign in"
+                  ) : (
+                    "Sign up"
+                  )}
+                </Button>
+              </div>
             </form>
           </Form>
+          <footer className="flex justify-center gap-1">
+            <p className="text-14 font-normal text-gray-600">
+              {type === "sign-in"
+                ? "Dont have an account?"
+                : "Already have an account?"}
+            </p>
+            <Link
+              href={type === "sign-in" ? "/sign-up" : "/sign-in"}
+              className="form-link"
+            >
+              {type === "sign-in" ? "Sign up" : "Sign-in"}
+            </Link>
+          </footer>
         </>
       )}
     </section>
